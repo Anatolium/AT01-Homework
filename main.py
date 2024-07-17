31334a739304a3b09c4027e5bb9ca83de5d5acc4
Анатолий <lan2002@yandex.ru>
2024-07-17 16:41:33 +0300

    (new) main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# def add(a, b):
+#     return a + b
+#
+#
+# def subtract(a, b):
+#     return a - b
+#
+#
+# def multiply(a, b):
+#     return a * b
+#
+#
+# def divide(a, b):
+#     return a / b
+#
+#
+# def is_even(number):
+#     return number % 2 == 0
+#
+#
+# def divide(a, b):
+#     if b == 0:
+#         raise ValueError('На ноль делить нельзя')
+#     return a / b
+
+
+def remainder(dividend, divisor):
+    if divisor == 0:
+        raise ValueError("Делитель не может быть равен нулю")
+    return dividend % divisor
+
